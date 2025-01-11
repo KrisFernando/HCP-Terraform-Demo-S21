97d402edb46120e4f6cd2943799fea52038a18b5
Kris Fernando <114943416+KrisFernando@users.noreply.github.com>
2025-01-11 07:17:17 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,11 @@ terraform {
   }
 }
 
-provider "hcp" {}
+provider "hcp" {
+  workload_identity {
+    resource_name = "iam/project/d8df60a8-99d8-4876-a00d-fea2f53133bd/service-principal/cha-service-principle"
+  }
+}
 
 provider "aws" {
   region = "us-east-2"
